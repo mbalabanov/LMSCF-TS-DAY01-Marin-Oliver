c3abdcc4785590701a725fe4804de6e3b3435c57
Marin Balabanov <marincomics@gmail.com>
2020-02-18 15:15:49 +0100

    Adpated JSON to array, translated JS to TS

--- 02_advanced_ts/js/myscript.js
+++ 02_advanced_ts/js/myscript.js
@@ -1,95 +1,104 @@
-
-/* Initializing variables */
-let sortByLikes = false;
-let foodListContent = '';
-let sequence = [];
-
-/* Generates the movie list by checking the settings for sorting.
-    If "sort by likes" is selected, the array in the variable with the JSON data is sorted by the number of likes.
-    If "unsorted" is selected, then the array in the variable with the JSON data is sorted by item ID.
-    Then the JSON data is iterated. The constructed HTML is stored in a variable and rendering it into the div.
-    Each movie item has an accompanying modal that is invisible to the user, until they click on a movie.*/
+var menudata = [
+    {
+        id: 0,
+        name: "Tafelspitz",
+        info: "Der Tafelspitz ist ein Gericht der Wiener Küche, dessen Bezeichnung bayerisch-österreichische Wurzeln hat.",
+        image: "img/01_tafelspitz.jpg",
+        likes: "0"
+    },
+    {
+        id: 1,
+        name: "Wiener Schnitzel mit Erdapfelsalat",
+        info: "Wiener Schnitzel ist ein dünnes, paniertes und ausgebackenes Schnitzel aus Kalbfleisch. Es gehört zu den bekanntesten Spezialitäten der Wiener Küche.",
+        image: "img/02_wiener_schnitzel.jpg",
+        likes: 0
+    },
+    {
+        id: 2,
+        name: "Fiakergulasch",
+        info: "Eine üppigere Abwandlung des Wiener Gulaschs ist das Fiakergulasch, das zusätzlich mit gebratenen oder frittierten Frankfurter Würsteln, Spiegelei, fächerartig geschnittener Essiggurke und eventuell Semmelknödeln serviert wird.",
+        image: "img/03_fiakergulasch.jpg",
+        likes: 0
+    },
+    {
+        id: 3,
+        name: "Leberkässemmel",
+        info: "Als Fleischkäse, Leberkäse (auch -kas) oder Fleischlaib bezeichnet man eine Brühwurstsorte[1]. Charakteristisch ist seine eckige Pastetenform. Leber findet bei der Herstellung zumeist keine Verwendung. Die Bezeichnung als Käse leitet sich lediglich von der Form der Laibe ab.",
+        image: "img/04_leberkassemmel.jpg",
+        likes: 0
+    },
+    {
+        id: 4,
+        name: "Geröstete Knödel mit Ei",
+        info: "Geröstete Knödel mit Ei ein vegetarisches Gericht und eine schmackhafte Resteverwertung, als Snack oder als Hauptspeise, mit Salat als Beilage schmecken sie besonders gut.",
+        image: "img/05_geroestete_knoedel.jpg",
+        likes: 0
+    },
+    {
+        id: 5,
+        name: "Naturschnitzel mit Reis",
+        info: "Naturschnitzel zählen zu den klassischen Gerichten der Wiener Küche. Das Fleisch wird in Butter gebraten und dann mit Fond aufgegossen. So bekommt man einen schmackhaften Buttersaft. Es ist sehr einfach zu zubereiten, trotzdem muss man sorgsam vorgehen und wirklich nur gute Fleischqualität wählen, sonst wird das Gericht leicht zäh.",
+        image: "img/06_naturschnitzel.jpg",
+        likes: 0
+    },
+    {
+        id: 6,
+        name: "Geröstete Leber mit Sauce Tartare",
+        info: "Aus der Wiener Küche waren Geröstete Leber und „Beuschel“ sowieso nie wegzudenken. Die Spuren der „Gerösteten Leber“ lassen sich bis in die gutbürgerliche Küche des 19. Jahrhunderts zurückverfolgen. Zart gekocht und mit feinen Nuancen zahlreicher Gewürze durchzogen, ist sie auch heute noch sehr beliebt, schmeckt herzhaft und ist definitiv ein Wiener Kulturgut.",
+        image: "img/07_eiernockerln.jpg",
+        likes: 0
+    },
+    {
+        id: 7,
+        name: "Eiernockerl mit Salat",
+        info: "lassisch und gut: Eiernockerl sind ein traditionelles österreichisches Gericht und ganz einfach selbst zuzubereiten. Unverzichtbar ist dabei der grüne Salat.",
+        image: "img/08_geroestete-leber.jpg",
+        likes: 0
+    }
+];
+var sortByLikes = false;
+var foodListContent = '';
+var sequence = [];
 function generateFood() {
     foodListContent = '';
-    $('#foodcards').replaceWith(`<div class="row text-center" id="foodcards"></div>`);
+    $('#foodcards').replaceWith("<div class=\"row text-center\" id=\"foodcards\"></div>");
     if (sortByLikes == true) {
         sequence = [];
-        menudata.food.sort(function(a, b) { return b.likes - a.likes });
-        for (let sortThrough in menudata.food) {
-            sequence.push(menudata.food[sortThrough].id);
+        menudata.sort(function (a, b) { return b.likes - a.likes; });
+        for (var sortThrough in menudata) {
+            sequence.push(menudata[sortThrough].id);
         }
-    } else if (sortByLikes == false) {
+    }
+    else if (sortByLikes == false) {
         sequence = [];
-        menudata.food.sort(function(c, d) { return c.id - d.id });
-        for (let sortBy in menudata.food) {
-            sequence.push(menudata.food[sortBy].id);
+        menudata.sort(function (c, d) { return c.id - d.id; });
+        for (var sortBy in menudata) {
+            sequence.push(menudata[sortBy].id);
         }
-    };
-    for (let tempID in sequence) {
-        foodListContent += `
-        <div class="card text-white m-4 p-1 col-lg-5 col-md-5 col-sm-4 bg-warning">
-            <div class="row no-gutters">
-                <div class="col-lg-5 col-md-5 col-sm-12">
-                    <img src="${menudata.food[tempID].image}" class="card-img" alt="${menudata.food[tempID].name}" data-toggle="modal" data-target="#modal${menudata.food[tempID].id}">
-                </div>
-                <div class="col-lg-7 col-md-7 col-sm-12">
-                    <div class="card-body text-left">
-                        <h5 class="card-title" data-toggle="modal" data-target="#modal${menudata.food[tempID].id}">${menudata.food[tempID].name}</h5>
-                        <p class="card-text text-truncate" data-toggle="modal" data-target="#modal${menudata.food[tempID].id}"><small>${menudata.food[tempID].info}</small></p>
-                    </div>
-                    <div class="card-footer">
-                        <h4 class="card-text text-right text-dark likeMe" id="${tempID}"><small>Like&nbsp;<i class="fa fa-thumbs-up"></i></small>&nbsp;&nbsp;<span class="badge badge-dark rounded-circle">${menudata.food[tempID].likes}</span></h4>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="modal fade" id="modal${menudata.food[tempID].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered" role="document">
-                <div class="modal-content bg-secondary text-white">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLabel">${menudata.food[tempID].name}</h5>
-                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div class="modal-body">
-                        <p><img src="${menudata.food[tempID].image}" class="img-fluid close" data-dismiss="modal" alt="${menudata.food[tempID].name}"></p>
-                        <h3>${menudata.food[tempID].name}</h3>
-                        <p>${menudata.food[tempID].info}</p>
-                    </div>
-                </div>
-            </div>
-        </div>
-        `;
-    };
-    $('#foodcards').replaceWith(`<div class="row text-center" id="foodcards">${foodListContent}</div>`);
+    }
+    ;
+    for (var tempID in sequence) {
+        foodListContent += "\n        <div class=\"card text-white m-4 p-1 col-lg-5 col-md-5 col-sm-4 bg-warning\">\n            <div class=\"row no-gutters\">\n                <div class=\"col-lg-5 col-md-5 col-sm-12\">\n                    <img src=\"" + menudata[tempID].image + "\" class=\"card-img\" alt=\"" + menudata[tempID].name + "\" data-toggle=\"modal\" data-target=\"#modal" + menudata[tempID].id + "\">\n                </div>\n                <div class=\"col-lg-7 col-md-7 col-sm-12\">\n                    <div class=\"card-body text-left\">\n                        <h5 class=\"card-title\" data-toggle=\"modal\" data-target=\"#modal" + menudata[tempID].id + "\">" + menudata[tempID].name + "</h5>\n                        <p class=\"card-text text-truncate\" data-toggle=\"modal\" data-target=\"#modal" + menudata[tempID].id + "\"><small>" + menudata[tempID].info + "</small></p>\n                    </div>\n                    <div class=\"card-footer\">\n                        <h4 class=\"card-text text-right text-dark likeMe\" id=\"" + tempID + "\"><small>Like&nbsp;<i class=\"fa fa-thumbs-up\"></i></small>&nbsp;&nbsp;<span class=\"badge badge-dark rounded-circle\">" + menudata[tempID].likes + "</span></h4>\n                    </div>\n                </div>\n            </div>\n        </div>\n        <div class=\"modal fade\" id=\"modal" + menudata[tempID].id + "\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalLabel\" aria-hidden=\"true\">\n            <div class=\"modal-dialog modal-dialog-centered\" role=\"document\">\n                <div class=\"modal-content bg-secondary text-white\">\n                    <div class=\"modal-header\">\n                        <h5 class=\"modal-title\" id=\"exampleModalLabel\">" + menudata[tempID].name + "</h5>\n                        <button type=\"button\" class=\"close\" data-dismiss=\"modal\" aria-label=\"Close\">\n                        <span aria-hidden=\"true\">&times;</span>\n                        </button>\n                    </div>\n                    <div class=\"modal-body\">\n                        <p><img src=\"" + menudata[tempID].image + "\" class=\"img-fluid close\" data-dismiss=\"modal\" alt=\"" + menudata[tempID].name + "\"></p>\n                        <h3>" + menudata[tempID].name + "</h3>\n                        <p>" + menudata[tempID].info + "</p>\n                    </div>\n                </div>\n            </div>\n        </div>\n        ";
+    }
+    ;
+    $('#foodcards').replaceWith("<div class=\"row text-center\" id=\"foodcards\">" + foodListContent + "</div>");
     setEventListeners();
-};
-
-/* This sets the event listeners to the "like" buttons after the HTML is rendered.
-    When a user clicks on "like", then the ID of the movie is read.
-    The number of likes the variable containing the JSON data are incremented.
-    The movie list is rendered into the HTML again to reflect the new number of likes.
-    This rendering also changes the order of the items, if "sort by likes" is selected. */
+}
+;
 function setEventListeners() {
-    $('.likeMe').click(function() {
-        menudata.food[this.id].likes ++;
+    $('.likeMe').click(function () {
+        menudata[this.id].likes++;
         generateFood();
     });
-};
-
-/* When a user selects a sorting setting in the dropdown menu, the sortByLikes variable is set to TRUE. */
-$('#sortLikesAscending').click(function() {
+}
+;
+$('#sortLikesAscending').click(function () {
     sortByLikes = true;
     generateFood();
-    $('#sortStatus').replaceWith(`<div id="sortStatus"><small>Sorted by likes</small></div>`);
+    $('#sortStatus').replaceWith("<div id=\"sortStatus\"><small>Sorted by likes</small></div>");
 });
-
-/* When a user selects a sorting setting in the dropdown menu, the sortByLikes variable is set to FALSE. */
-$('#doNotSort').click(function() {
+$('#doNotSort').click(function () {
     sortByLikes = false;
     generateFood();
-    $('#sortStatus').replaceWith(`<div id="sortStatus"><small>Unsorted</small></div>`);
+    $('#sortStatus').replaceWith("<div id=\"sortStatus\"><small>Unsorted</small></div>");
 });
-
-/* Exercise solved by Marin on Saturday, February 15, 2020 */
---
+++ 02_advanced_ts/js/myscript.ts
@@ -0,0 +1,139 @@
+
+let menudata: any[] = [
+        {
+        id: 0,
+        name: "Tafelspitz",
+        info: "Der Tafelspitz ist ein Gericht der Wiener Küche, dessen Bezeichnung bayerisch-österreichische Wurzeln hat.",
+        image: "img/01_tafelspitz.jpg",
+        likes: "0"
+       },
+       {
+        id: 1,
+        name: "Wiener Schnitzel mit Erdapfelsalat",
+        info: "Wiener Schnitzel ist ein dünnes, paniertes und ausgebackenes Schnitzel aus Kalbfleisch. Es gehört zu den bekanntesten Spezialitäten der Wiener Küche.",
+        image: "img/02_wiener_schnitzel.jpg",
+        likes: 0
+        },
+        {
+        id: 2,
+        name: "Fiakergulasch",
+        info: "Eine üppigere Abwandlung des Wiener Gulaschs ist das Fiakergulasch, das zusätzlich mit gebratenen oder frittierten Frankfurter Würsteln, Spiegelei, fächerartig geschnittener Essiggurke und eventuell Semmelknödeln serviert wird.",
+        image: "img/03_fiakergulasch.jpg",
+        likes: 0
+        },
+        {
+        id: 3,
+        name: "Leberkässemmel",
+        info: "Als Fleischkäse, Leberkäse (auch -kas) oder Fleischlaib bezeichnet man eine Brühwurstsorte[1]. Charakteristisch ist seine eckige Pastetenform. Leber findet bei der Herstellung zumeist keine Verwendung. Die Bezeichnung als Käse leitet sich lediglich von der Form der Laibe ab.",
+        image: "img/04_leberkassemmel.jpg",
+        likes: 0
+        },
+        {
+        id: 4,
+        name: "Geröstete Knödel mit Ei",
+        info: "Geröstete Knödel mit Ei ein vegetarisches Gericht und eine schmackhafte Resteverwertung, als Snack oder als Hauptspeise, mit Salat als Beilage schmecken sie besonders gut.",
+        image: "img/05_geroestete_knoedel.jpg",
+        likes: 0
+        },
+        {
+        id: 5,
+        name: "Naturschnitzel mit Reis",
+        info: "Naturschnitzel zählen zu den klassischen Gerichten der Wiener Küche. Das Fleisch wird in Butter gebraten und dann mit Fond aufgegossen. So bekommt man einen schmackhaften Buttersaft. Es ist sehr einfach zu zubereiten, trotzdem muss man sorgsam vorgehen und wirklich nur gute Fleischqualität wählen, sonst wird das Gericht leicht zäh.",
+        image: "img/06_naturschnitzel.jpg",
+        likes: 0
+        },
+        {
+        id: 6,
+        name: "Geröstete Leber mit Sauce Tartare",
+        info: "Aus der Wiener Küche waren Geröstete Leber und „Beuschel“ sowieso nie wegzudenken. Die Spuren der „Gerösteten Leber“ lassen sich bis in die gutbürgerliche Küche des 19. Jahrhunderts zurückverfolgen. Zart gekocht und mit feinen Nuancen zahlreicher Gewürze durchzogen, ist sie auch heute noch sehr beliebt, schmeckt herzhaft und ist definitiv ein Wiener Kulturgut.",
+        image: "img/07_eiernockerln.jpg",
+        likes: 0
+        },
+        {
+        id: 7,
+        name: "Eiernockerl mit Salat",
+        info: "lassisch und gut: Eiernockerl sind ein traditionelles österreichisches Gericht und ganz einfach selbst zuzubereiten. Unverzichtbar ist dabei der grüne Salat.",
+        image: "img/08_geroestete-leber.jpg",
+        likes: 0
+        }
+    ];
+
+let sortByLikes: boolean = false;
+let foodListContent: string = '';
+let sequence: any = [];
+
+function generateFood() {
+    foodListContent = '';
+    $('#foodcards').replaceWith(`<div class="row text-center" id="foodcards"></div>`);
+    if (sortByLikes == true) {
+        sequence = [];
+        menudata.sort(function(a, b) { return b.likes - a.likes });
+        for (let sortThrough in menudata) {
+            sequence.push(menudata[sortThrough].id);
+        }
+    } else if (sortByLikes == false) {
+        sequence = [];
+        menudata.sort(function(c, d) { return c.id - d.id });
+        for (let sortBy in menudata) {
+            sequence.push(menudata[sortBy].id);
+        }
+    };
+    for (let tempID in sequence) {
+        foodListContent += `
+        <div class="card text-white m-4 p-1 col-lg-5 col-md-5 col-sm-4 bg-warning">
+            <div class="row no-gutters">
+                <div class="col-lg-5 col-md-5 col-sm-12">
+                    <img src="${menudata[tempID].image}" class="card-img" alt="${menudata[tempID].name}" data-toggle="modal" data-target="#modal${menudata[tempID].id}">
+                </div>
+                <div class="col-lg-7 col-md-7 col-sm-12">
+                    <div class="card-body text-left">
+                        <h5 class="card-title" data-toggle="modal" data-target="#modal${menudata[tempID].id}">${menudata[tempID].name}</h5>
+                        <p class="card-text text-truncate" data-toggle="modal" data-target="#modal${menudata[tempID].id}"><small>${menudata[tempID].info}</small></p>
+                    </div>
+                    <div class="card-footer">
+                        <h4 class="card-text text-right text-dark likeMe" id="${tempID}"><small>Like&nbsp;<i class="fa fa-thumbs-up"></i></small>&nbsp;&nbsp;<span class="badge badge-dark rounded-circle">${menudata[tempID].likes}</span></h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal fade" id="modal${menudata[tempID].id}" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered" role="document">
+                <div class="modal-content bg-secondary text-white">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">${menudata[tempID].name}</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p><img src="${menudata[tempID].image}" class="img-fluid close" data-dismiss="modal" alt="${menudata[tempID].name}"></p>
+                        <h3>${menudata[tempID].name}</h3>
+                        <p>${menudata[tempID].info}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        `;
+    };
+    $('#foodcards').replaceWith(`<div class="row text-center" id="foodcards">${foodListContent}</div>`);
+    setEventListeners();
+};
+
+function setEventListeners() {
+    $('.likeMe').click(function() {
+        menudata[this.id].likes ++;
+        generateFood();
+    });
+};
+
+$('#sortLikesAscending').click(function() {
+    sortByLikes = true;
+    generateFood();
+    $('#sortStatus').replaceWith(`<div id="sortStatus"><small>Sorted by likes</small></div>`);
+});
+
+$('#doNotSort').click(function() {
+    sortByLikes = false;
+    generateFood();
+    $('#sortStatus').replaceWith(`<div id="sortStatus"><small>Unsorted</small></div>`);
+});
